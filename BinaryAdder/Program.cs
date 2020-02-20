1ecf8eb4e34b5c38813ffefd4d82b10734c43121
chrizz92 <christianschadler@gmx.at>
2020-02-20 16:32:11 +0100

    fertig

--- BinaryAdder/Program.cs
+++ BinaryAdder/Program.cs
@@ -26,6 +26,7 @@ namespace BinaryAdder
                     if (inputNumber[i] < '0' || inputNumber[i] > '1')
                     {
                         isNumber = false;
+                        Console.WriteLine("Nur 1 und 0 sind in Binärzahlen erlaubt!");
                         break;
                     }
                 }
@@ -56,11 +57,11 @@ namespace BinaryAdder
         }
 
         /// <summary>
-        /// Takes two integer numbers as strings and makes an addition with them.
+        /// Takes two binary numbers as strings and makes an addition with them.
         /// </summary>
         /// <param name="number1"></param>
         /// <param name="number2"></param>
-        /// <returns>A string with the result of the integer addition</returns>
+        /// <returns>A string with the result of the binary addition</returns>
         public static string AddBigBinaries(string number1, string number2)
         {
             string result = "";
@@ -82,12 +83,80 @@ namespace BinaryAdder
                 }
             }
 
-            //TODO Umschreiben auf Dualzahl
+            //Building of the result string
             for (int i = number1.Length - 1; i >= 0; i--)
             {
-                
+                if (increaseNextDigit)
+                {
+                    if(number1[i] == '1' && number2[i] == '1')
+                    {
+                        result = "1" + result;
+                    }
+                    else if(number1[i]=='0' && number2[i] == '0')
+                    {
+                        result = "1" + result;
+                        increaseNextDigit = false;
+                    }
+                    else
+                    {
+                        result = "0" + result;
+                    }
+                }
+                else
+                {
+                    if (number1[i] == '1' && number2[i] == '1')
+                    {
+                        result = "0" + result;
+                        increaseNextDigit = true;
+                    }
+                    else if (number1[i] == '0' && number2[i] == '0')
+                    {
+                        result = "0" + result;
+                    }
+                    else
+                    {
+                        result = "1" + result;
+                    }
+                }
             }
 
+            if (increaseNextDigit)
+            {
+                result = "1" + result;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Takes a string as a potential binary number and tries to make 32-bit-integer out of it. If the string is not a binary number the function returns zero.
+        /// </summary>
+        /// <param name="binaryNumber"></param>
+        /// <returns>The integer value of the given binary number</returns>
+        public static int ConvertBinaryNumberToInt32(string binaryNumber)
+        {
+            int result = 0, power = 2;
+
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                if (binaryNumber[i] < '0' || binaryNumber[i] > '1')
+                {
+                    result = 0;
+                    break;
+                }
+                else if (binaryNumber[i] == '1' && i < binaryNumber.Length - 1)
+                {
+                    power = 2;
+                    for (int j = 0; j < binaryNumber.Length-2-i; j++)
+                    {
+                        power *= 2;
+                    }
+                    result += power;
+                }
+                else if (binaryNumber[i] == '1' && i == binaryNumber.Length - 1)
+                {
+                    result += 1;
+                }
+            }
             return result;
         }
 
@@ -96,7 +165,17 @@ namespace BinaryAdder
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Binäraddierer");
+            Console.WriteLine("=============");
+
+            string binaryNumberOne = ReadBinaryNumber(1);
+            string binaryNumberTwo = ReadBinaryNumber(2);
+            string binaryNumberResult = AddBigBinaries(binaryNumberOne, binaryNumberTwo);
+            Console.WriteLine("Addition im Binärsystem:");
+            Console.WriteLine("{0} + {1} = {2}",binaryNumberOne,binaryNumberTwo,binaryNumberResult);
+            Console.WriteLine();
+            Console.WriteLine("Addition im Dezimalsystem:");
+            Console.WriteLine("{0} + {1} = {2}", ConvertBinaryNumberToInt32(binaryNumberOne), ConvertBinaryNumberToInt32(binaryNumberTwo), ConvertBinaryNumberToInt32(binaryNumberResult));
         }
     }
 }
